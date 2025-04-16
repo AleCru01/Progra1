cea72549318b046555753ead18a472509344f1c6
AleCru01 <victorbarrios1011@gmail.com>
2025-04-16 15:15:39 -0600

    a
    añadi bloque de texto y países en cada confederación

--- Proyecto/Recursos/Clasificatorias.h
+++ Proyecto/Recursos/Clasificatorias.h
@@ -1,28 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h> //librería para definir la función Exit(1) y terminar el programa
+
 
 //proceso de abrir los documentos:
 void InicioClasificacion(){
 	//proceso de leer los archivos .txt donde estan la lista de los paises y su ranking fifa
+	
 	FILE *CONCA = fopen("Recursos/Confederacion/CONCACAF.txt", "r"); //america del norte
 	FILE *UEFA = fopen("Recursos/Confederacion/UEFA.txt", "r"); //Europa
 	FILE *CONME = fopen("Recursos/Confederacion/CONMEBOL.txt", "r"); //Sudamerica
 	FILE *AFC = fopen("Recursos/Confederacion/AFC.txt", "r"); //Asia 
+	FILE *CAF = fopen("Recursos/Confederacion/CAF.txt", "r"); //Africa
+	FILE *OFC = fopen("Recursos/Confederacion/OFC.txt", "r"); //Oceania
+	
+	// Verificar si alguno no se abrió
+	
+	/*
+		FICHA TÉCNICA: 
+			Use condicionales NEGATIVAS para verificar si algun documento no existe
+			basicamente: 
+			!CAF  -> es lo mismo a -> CAF == NULL
+	*/
+	if (!CONCA) { printf("ARCHIVO FALTANTE: CONCACAF.txt\n\n"); exit(1); }
+    if (!UEFA)  { printf("ARCHIVO FALTANTE: UEFA.txt\n\n");      exit(1); }
+    if (!CONME) { printf("ARCHIVO FALTANTE: CONMEBOL.txt\n\n");  exit(1); }
+    if (!AFC)   { printf("ARCHIVO FALTANTE: AFC.txt\n\n");       exit(1); }
+    if (!CAF)   { printf("ARCHIVO FALTANTE: CAF.txt\n\n");       exit(1); }
+    if (!OFC)   { printf("ARCHIVO FALTANTE: OFC.txt\n\n");       exit(1); }
 	
 	
-	//NOTIFICACIÓN DE QUE FALTA UN ARCHIVO EN ESPECIFICO
-	if(CONCA == NULL){
-		printf("ARCHIVO FALTANTES: CONCACAF.txt\n\n");
-	}else if(UEFA == NULL){
-		printf("ARCHIVO FALTANTES: UEFA.txt\n\n");
-	}else if(CONME == NULL){
-		printf("ARCHIVO FALTANTES: CONME.txt\n\n");
-	}else if(AFC == NULL){
-		printf("ARCHIVO FALTANTES: AFC.txt\n\n");
-	}
-	else{
-		while(feof(AFC) == 0){			
-			char caracter = fgetc(AFC);
-			printf("%c", caracter);
-		}
-	}
+	int confederaciones[6] = {*CONCA, *UEFA, *CONME, *AFC,*CAF, *OFC};	
+	
+	
+	// cerrar los archivos al final:
+    fclose(CONCA);
+    fclose(UEFA);
+    fclose(CONME);
+    fclose(AFC);
+    fclose(CAF);
+    fclose(OFC);
 }
