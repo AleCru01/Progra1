d514093a8272aee652cd137cf8373fe58bd83122
AleCru01 <victorbarrios1011@gmail.com>
2025-05-12 07:51:16 -0600

    avance de numero aleatorio

--- Practicas y Evidencias/Ejercicio numero aleatorio.cpp
+++ Practicas y Evidencias/Ejercicio numero aleatorio.cpp
@@ -8,18 +8,25 @@ int buscarIndice(long numeroX, int arreglo[]);
 
 void sumatoria(int longitud, int ranking[], int cupos, int repechaje){
 	long PuntosTotalesRankings = 0;
+	int LugaresOcupados[cupos]; //array que nos guarda los indices ya ocupados
 	int i;
 	for(i = 0; i< longitud; i++){
 		PuntosTotalesRankings += ranking[i];
 	}
 	printf("Puntos totales de todo el arreglo: %d\n\n", PuntosTotalesRankings);
+	
+	//para obtener los cupos
 	for(i = 1; i<=cupos; i++){
 		//quiero pasar como parametro el return de numeroAleatorio
-		int numero = numeroAleatorio(ranking,PuntosTotalesRankings);
+		long numero = numeroAleatorio(ranking,PuntosTotalesRankings);
 		printf("Numero aleatorio a buscar: %d\n", numero);
-		int indice = buscarIndice(numero, ranking);
+		int indice = buscarIndice(numero, ranking); //guardo el indice en el arrary para luego restarlo y que no salga de nuevo
+		LugaresOcupados[i-1] = indice;
 		printf("El indice que corresponde (el mas cercano) es: %d\n", indice);
 		printf("el valor de ese indice es: %d\n\n\n", ranking[indice]);
+		
+		//ahora hay que restar el valor del ranking y que no salga 
+		PuntosTotalesRankings -= LugaresOcupados[i-1];
 	}
 	
 	
@@ -29,6 +36,8 @@ long numeroAleatorio(int cadena[], long rango){
 	int aleatorio =1 + rand() % rango;
 	return aleatorio;
 }
+
+
 int buscarIndice(long numeroX, int arreglo[]){
 	int i = 0;
 	long PuntosAcumulados=arreglo[i];
@@ -36,6 +45,12 @@ int buscarIndice(long numeroX, int arreglo[]){
 		i++;
 		PuntosAcumulados+=arreglo[i];
 	}while(PuntosAcumulados < numeroX);
+	
+	//me falta poner alguna condicion de que verifique que el valor de i esta en el array de LugaresOcupados
+	//y si esta, volver a generar otro numero aleatorio, y usando recursividad volver a buscar el numero.
+	
+	
+	
 	
 	//retornara el Indice del arreglo que en donde estara; 
 	return i;
